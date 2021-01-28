65e51ec8076591dcd817a2a338af8e8ad6b3e59e
ChicomeCipactli <chicomecipactli@gmail.com>
2021-01-27 18:47:51 -0600

    still adding to path

--- install.sh
+++ install.sh
@@ -1,1 +1,7 @@
-cp Scripts/CopyLatexExample.sh /usr/local/bin
+#! /bin/bash
+
+proj_d=Latex-Configuracion-Manual
+path_to_scripts=~/$proj_d/Scripts
+
+echo "export PATH=$PATH:$path_to_scripts" >> ~/.bashrc
+echo "export PATH=$PATH:$path_to_scripts" >> ~/.zshrc
--- uninstall.sh
+++ uninstall.sh
@@ -1,1 +1,5 @@
-rm /usr/local/bin/CopyLatexExample.sh
+#! /bin/bash
+
+proj_d=~/bin/Latex-Configuracion-Manual
+
+rm -rf $proj_d
