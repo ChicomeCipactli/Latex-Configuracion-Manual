a5052e4863820a7f3b9b161379c750824d810491
ChicomeCipactli <chicomecipactli@gmail.com>
2021-02-01 13:40:29 -0600

    Mejorar el makefile

--- Scripts/Compile/Tarea1.sh
+++ Scripts/Compile/Tarea1.sh
@@ -1,17 +1,50 @@
-pdflatex *.tex
+#! /bin/bash
 
-if [ ! -d build ] ;
-then 
-    mkdir build
+archivo=main.tex
+
+for item in * 
+do
+    if [ ${item##*.} == tex ] ; then
+        archivo=$item
+    fi
+done    
+
+name=${archivo%.*}
+
+compile=$(pdflatex ${name}.tex)
+
+# if [ ! -d pdfs ] ;
+# then 
+#     echo -e "[\e[34mkdir\e[0m pdfs]"
+#     mkdir pdfs
+# fi
+# 
+# if [ ! -d build ] ;
+# then 
+#     echo -e "[\e[34mkdir\e[0m build]"
+#     mkdir build
+# fi
+# 
+# if [ ! -d logs ] ; then 
+#     echo -e "[\e[34mkdir\e[0m logs]"
+#     mkdir logs
+# fi
+
+echo -e "[COMPILING \e[33m\e[1m${name}.tex\e[0m]"
+# if ${compile} > logs/${name}.log 2>&1 ; then
+if ${compile} ; then
+    # cat ${name}.log
+    echo -e "[\e[34m\e[1mDONE\e[0m]"
+else
+    ${compile}
+    # cat ${name}.log
+    echo -e "[\e[34m\e[1mFINISHED WITH ERROR\e[0m]"
 fi
 
-mv *.aux build
-mv *.out build
-mv *.log build
+# evince ${name}.pdf &
 
-if [ ! -d pdfs ] ;
-then 
-    mkdir pdfs
-fi
-
-mv *.pdf pdfs
+# mv ${name}.aux build
+# mv ${name}.out build
+# mv ${name}.log logs
+# 
+# mv ${name}.pdf pdfs
--- Scripts/Ver.sh
+++ Scripts/Ver.sh
@@ -1,10 +1,24 @@
-if [ -e pdfs/*.pdf ] ;
+#! /bin/bash
+
+archivo=main.tex
+
+for item in * 
+do
+    if [ ${item##*.} == tex ] ; then
+        archivo=$item
+    fi
+done    
+
+name=${archivo%.*}
+
+if [ -e ${name}.pdf ] ; then
+    evince ${name}.pdf &
+elif [ -e pdfs/*.pdf ] ;
 then
-    evince pdfs/*.pdf &
-elif [ -e ../pdfs/*.pdf ]
+    evince pdfs/${name}.pdf &
+elif [ -e ../pdfs/${name}.pdf ] ; 
 then
-    evince pdfs/*.pdf &
+    evince pdfs/${name}.pdf &
 else
     echo -e "[\e[31mPDF\e[0m not found]"
 fi
-
